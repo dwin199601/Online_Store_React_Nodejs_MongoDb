e1b1dca9adf085a97464749562b8c68d21e73e73
Oleksandr Dolotii <dwin13672@gmail.com>
2021-12-05 13:12:24 -0500

    update1

--- src/App.js
+++ src/App.js
@@ -5,8 +5,44 @@ import Home from './components/home';
 import Items from './components/items';
 import Newitems from './components/newitems';
 import ItemDetails from './components/itemDetails';
+import UpdateItem from './components/updateItem';
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+
+
+
 
 function App() {
+
+
+  const [item, setItem] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=> {
+   fetch('http://localhost:5000/api/items')
+   .then(res=>{
+       if(!res.ok){
+           throw Error (`Couldn't fetch the data from the server!`)
+       }
+       console.log("Server works!!")
+       return res.json();
+      
+   })
+   .then(data=>{//to process data from the json file
+       setItem(data);
+       console.log(item)
+       setLoading(false);
+       
+   })
+   .catch(err=>{
+       console.log("Error: " + err);
+       setLoading(false);
+   })
+   }, []);
+
+   
+
   return (
     <Router>
     <div className="App">
@@ -14,11 +50,14 @@ function App() {
       <div className="content">
        <Switch>
 
-       <Route exact path="/">
-        <Home/>
-        </Route>
+       <Route exact path="/"  render={()=> <Home 
+                                        item = {item} 
+                                        setLoading = {setLoading}
+                                        loading={loading}
 
-        <Route path="/newitems">
+                                    />} />
+
+        <Route path="/newitems" >
           <Newitems/>
         </Route>
 
@@ -26,16 +65,15 @@ function App() {
          <ItemDetails/>
         </Route>
         
-        <Route path="/items">
-          <Items/>
+        <Route path="/items" >
+          <Items />
         </Route>
-
+         
+        <Route path="/updateItem/:param">
+          <UpdateItem/>
+        </Route>
       
-
-       
-
-       
-
+     
         </Switch>
         </div> 
     </div>
